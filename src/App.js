191bc098990924ffa49bf6bfa5c141ec6922aa4d
moses <mosesblair00@gmail.com>
2023-01-30 20:10:19 +0300

    tours project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://mosesblair12345.github.io/api/tour.json";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => {
+      return tour.id !== id;
+    });
+    setTours(newTours);
+  };
+
+  const getPlaces = async () => {
+    try {
+      const response = await fetch(url);
+      const places = await response.json();
+      setLoading(false);
+      setTours(places);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPlaces();
+  }, []);
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button className="btn" onClick={() => getPlaces()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      {loading ? <Loading /> : <Tours tours={tours} removeTour={removeTour} />}
+    </main>
+  );
+}
+
+export default App;
